4b445314012a7fcc05a1b529167ecf1b1d71cd45
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2020-07-01 17:48:30 +0100

    One layout class with size restrictions

--- src/main/java/jobicade/gooey/layout/Layout.java
+++ src/main/java/jobicade/gooey/layout/Layout.java
@@ -1,55 +1,93 @@
 package jobicade.gooey.layout;
 
-import java.util.Collections;
-import java.util.EnumSet;
-import java.util.Set;
+import com.google.common.collect.Range;
 
+import jobicade.gooey.GooeyMath;
 import jobicade.gooey.geom.Point;
 import jobicade.gooey.geom.Rect;
 
 /**
  * Tracks the position and size of a movable rectangular object.
  */
-public class Layout {
-    protected Rect bounds;
-
-    private final Point initialSize;
-    private final Set<Flag> flags;
-
-    public enum Flag {
-        SHRINK_X, SHRINK_Y,
-        GROW_X, GROW_Y
+public final class Layout {
+    /**
+     * @return A new layout accepting a fixed size.
+     */
+    public static Layout fixedSize(Point size) {
+        return new Layout(size, Range.singleton(size.getX()), Range.singleton(size.getY()));
     }
 
-    public Layout(Point size, Flag... flags) {
-        this.bounds = Rect.bySize(Point.ZERO, size);
-
-        this.initialSize = size;
-        this.flags = EnumSet.noneOf(Flag.class);
-        Collections.addAll(this.flags, flags);
+    /**
+     * @return A new layout accepting any size with a size hint.
+     */
+    public static Layout anySize(Point sizeHint) {
+        return new Layout(sizeHint, Range.atLeast(0), Range.atLeast(0));
     }
 
-    public void resize(Point size) {
-        if (size.getX() < initialSize.getX() && flags.contains(Flag.SHRINK_X)
-                || size.getX() > initialSize.getX() && flags.contains(Flag.GROW_X)) {
-            bounds = bounds.withWidth(size.getX());
-        } else {
-            bounds = bounds.withWidth(initialSize.getX());
+    /**
+     * @return A new layout accepting sizes within range and with a size hint.
+     */
+    public static Layout sizeRange(Point sizeHint, Range<Integer> widthRange, Range<Integer> heightRange) {
+        return new Layout(sizeHint, widthRange, heightRange);
+    }
+
+    private final Range<Integer> widthRange;
+    private final Range<Integer> heightRange;
+
+    private Layout(Point sizeHint, Range<Integer> widthRange, Range<Integer> heightRange) {
+        if (sizeHint.getX() < 0 || sizeHint.getY() < 0) {
+            throw new IllegalArgumentException("Negative size hint");
         }
 
-        if (size.getY() < initialSize.getY() && flags.contains(Flag.SHRINK_Y)
-                || size.getY() > initialSize.getY() && flags.contains(Flag.GROW_Y)) {
-            bounds = bounds.withHeight(size.getY());
-        } else {
-            bounds = bounds.withHeight(initialSize.getY());
+        this.sizeHint = sizeHint;
+        this.widthRange = widthRange;
+        this.heightRange = heightRange;
+
+        if (widthRange.isEmpty() || heightRange.isEmpty()) {
+            throw new IllegalArgumentException("Empty size range");
+        } else if (GooeyMath.lowerBound(widthRange) < 0 || GooeyMath.lowerBound(heightRange) < 0) {
+            throw new IllegalArgumentException("Negative size range");
+        } else if (!checkSize(sizeHint)) {
+            throw new IllegalArgumentException("Size hint out of range");
         }
     }
 
-    public void move(Point position) {
-        bounds = bounds.withPosition(position);
+    /**
+     * @return {@code true} if {@code size} is valid.
+     */
+    public boolean checkSize(Point size) {
+        return widthRange.contains(size.getX()) && heightRange.contains(size.getY());
     }
 
+    /**
+     * @return The closest valid size to {@code size}.
+     */
+    public Point clampSize(Point size) {
+        return new Point(GooeyMath.clamp(widthRange, size.getX()), GooeyMath.clamp(heightRange, size.getY()));
+    }
+
+    private final Point sizeHint;
+    /**
+     * The size hint should be chosen as the size of the layout if there are no
+     * further external decisions or constraints.
+     */
+    public Point getSizeHint() {
+        return sizeHint;
+    }
+
+    private Rect bounds;
     public Rect getBounds() {
         return bounds;
+    }
+
+    /**
+     * @throws IllegalArgumentException If {@code bounds} has an invalid size.
+     * @see #checkSize(Point)
+     */
+    public void setBounds(Rect bounds) {
+        if (!checkSize(bounds.getSize())) {
+            throw new IllegalArgumentException("Invalid size");
+        }
+        this.bounds = bounds;
     }
 }
